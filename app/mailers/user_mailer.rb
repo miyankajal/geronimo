86ec3ddd0e0d5e14366c892be1d73e1618e26aaf
Your Name <miyankajal@gmail.com>
2014-06-07 01:41:02 -0400

    fixed the parameters

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ActionMailer::Base
     mail(to: @user.email, subject: 'Welcome to Geronimo!!!')
   end
   
-  def min_points_email(user, student)
+  def min_points_email(user)
     @user = user
 	@student = student
     @url  = 'http://geronimo.com/login'
