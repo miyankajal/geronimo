fd814034b0a4193d134c1164f7575c2b4c5ae1eb
miyankajal <miyankajal@gmail.com>
2014-11-27 18:37:56 -0500

    Removed trailing slashes from the DNS

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ActionMailer::Base
  
   def welcome_email(user)
     @user = user
-    @url  = 'http://mygeronimo.com/'
+    @url  = 'http://mygeronimo.com'
     mail(to: @user.email, subject: 'Welcome to Geronimo!!!')
   end
   
   def appreciation_email(user, student, point)
       @user = user
       @student = student
-      @url  = 'http://mygeronimo.com/'
+      @url  = 'http://mygeronimo.com'
       @point = point
       mail(to: @user.email, subject: 'Student Appreciation Email')
   end
@@ -18,41 +18,41 @@ class UserMailer < ActionMailer::Base
   def min_points_email(user, student)
     @user = user
 	@student = student
-    @url  = 'http://mygeronimo.com/'
+    @url  = 'http://mygeronimo.com'
     mail(to: @user.email, subject: 'For Immediate Attention of the Guardian')
   end
   
   def too_many_email(user, student)
     @user = user
     @student = student
-    @url  = 'http://mygeronimo.com/'
+    @url  = 'http://mygeronimo.com'
     mail(to: @user.email, subject: 'For Immediate Attention of the Guardian')
   end
   
   def session_start_email(user)
     @user = user
-    @url  = 'http://mygeronimo.com/'
+    @url  = 'http://mygeronimo.com'
     mail(to: @user.email, subject: 'Session start')
   end
   
   def repetition_email(user, student, point)
     @user = user
     @student = student
-    @url  = 'http://mygeronimo.com/'
+    @url  = 'http://mygeronimo.com'
 	@point = point
     mail(to: @user.email, subject: 'For Immediate Attention of the Guardian')
   end
   
   def password_reset_email(user)
 	@user = user
-	@url  = 'http://mygeronimo.com/'
+	@url  = 'http://mygeronimo.com'
 	mail(to: @user.email, subject: 'Password Reset')
   end
   
   def pink_card_email(user, student, point)
     @user = user
     @student = student
-    @url  = 'http://mygeronimo.com/'
+    @url  = 'http://mygeronimo.com'
 	@point = point
     mail(to: @user.email, subject: 'For Immediate Attention of the Guardian')
   end
@@ -60,7 +60,7 @@ class UserMailer < ActionMailer::Base
   def yellow_card_email(user, student, point)
     @user = user
     @student = student
-    @url  = 'http://mygeronimo.com/'
+    @url  = 'http://mygeronimo.com'
 	@point = point
     mail(to: @user.email, subject: 'For Immediate Attention of the Guardian')
   end
@@ -68,20 +68,20 @@ class UserMailer < ActionMailer::Base
   def red_card_email(user, student, point)
     @user = user
     @student = student
-    @url  = 'http://mygeronimo.com/'
+    @url  = 'http://mygeronimo.com'
 	@point = point
     mail(to: @user.email, subject: 'For Immediate Attention of the Guardian')
   end
   
   def new_idea_email(user)
     @user = user
-    @url  = 'http://mygeronimo.com/'
+    @url  = 'http://mygeronimo.com'
     mail(to: @user.email, subject: 'New Idea on Geronimo!!!')
   end
   
   def report_idea_email(user)
     @user = user
-    @url  = 'http://mygeronimo.com/'
+    @url  = 'http://mygeronimo.com'
     mail(to: @user.email, subject: 'Flagged Idea on Geronimo!!!')
   end
   
